95a57f93fe84dc989c14d67c7e856d0005c81c08
Max Voronkin <voronkin.max.wm@gmail.com>
2022-08-19 11:38:18 +0300

    Created configuration extension, registered AuthService

---
+++ backend/Fung,API/ConfigurationExtensions/AppConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Fung.BLL.Services;
+using System.Runtime.CompilerServices;
+
+namespace Fung_API.ConfigurationExtensions
+{
+    public static class AppConfigurationExtensions
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<AuthService>();
+        }
+    }
+}
