7330ddbf44a877cb95f77607c555e0ea5f838d1b
Max Voronkin <voronkin.max.wm@gmail.com>
2022-08-22 15:05:21 +0300

    fixes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SharedModule } from './shared/shared/shared.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FungLogoComponent } from './fung-logo/fung-logo/fung-logo.component';
+import { FungLogoComponent } from './fung-logo/fung-logo.component';
 
 
 
