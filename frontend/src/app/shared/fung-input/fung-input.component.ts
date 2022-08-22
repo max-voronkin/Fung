999d040d6dc13e4e1108e5f6d1e21fed83c2fddd
Max Voronkin <voronkin.max.wm@gmail.com>
2022-08-22 21:02:29 +0300

    Created input template

---
+++ frontend/src/app/shared/fung-input/fung-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fung-input',
+  templateUrl: './fung-input.component.html',
+  styleUrls: ['./fung-input.component.sass']
+})
+export class FungInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
