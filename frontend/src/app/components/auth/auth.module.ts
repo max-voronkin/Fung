6c4f3a222de63a060a04edd0ca884f9e8ebd3944
Max Voronkin <voronkin.max.wm@gmail.com>
2022-08-23 14:36:35 +0300

    Created login page component

--- frontend/src/app/components/auth/auth.module.ts
+++ frontend/src/app/components/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { AuthPageComponent } from './auth-page/auth-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 
 @NgModule({
   declarations: [
     RegisterPageComponent,
-    AuthPageComponent
+    AuthPageComponent,
+    LoginPageComponent
   ],
   imports: [
     CommonModule, SharedModule
