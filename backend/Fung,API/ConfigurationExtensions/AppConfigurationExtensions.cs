2c0c97b1106fee9bd82934c7e29e9b1bcb59bda6
Max Voronkin <voronkin.max.wm@gmail.com>
2022-08-24 15:44:40 +0300

    Configured swagger

--- backend/Fung,API/ConfigurationExtensions/AppConfigurationExtensions.cs
+++ backend/Fung,API/ConfigurationExtensions/AppConfigurationExtensions.cs
@@ -4,6 +4,7 @@ using Fung.BLL.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -71,6 +72,8 @@ namespace Fung_API.ConfigurationExtensions
             services.ConfigureJwt(configuration);
             services.AddScoped<JwtFactory>();
 
+            services.AddSwagger();
+
             services.AddScoped<AuthService>();
             services.AddScoped<UserService>();
    
@@ -83,6 +86,41 @@ namespace Fung_API.ConfigurationExtensions
                 conf.AddProfile<UserMappingProfile>();
             },
             Assembly.GetExecutingAssembly()); 
+        }
+
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then " +
+                    "your token in the text input below.",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fung.API", Version = "v1" });
+            });
         }
     }
 }
