77c238664e10a85c56adb21380a92df03b6cb232
Max Voronkin <voronkin.max.wm@gmail.com>
2022-08-25 23:50:38 +0300

    Created jwt interceptor

---
+++ frontend/src/app/services/helpers/jwt.interceptor.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const accessToken = JSON.parse(localStorage.getItem('accessToken')!);
+
+    if (accessToken) {
+      request = request.clone({ setHeaders: { Authorization: `Bearer ${accessToken}` } });
+    }
+    return next.handle(request);
+  }
+}
