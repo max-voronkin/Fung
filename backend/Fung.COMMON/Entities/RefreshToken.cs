21a3a9b5daf8d8e4c5851c6bedb1341f3de052b8
Max Voronkin <voronkin.max.wm@gmail.com>
2022-08-26 20:02:31 +0300

    Added refresh token entity

---
+++ backend/Fung.COMMON/Entities/RefreshToken.cs
@@ -0,0 +1,16 @@
+﻿namespace Fung.COMMON.Entities
+{
+    public sealed class RefreshToken
+    {
+        public RefreshToken()
+        {
+            Expires = DateTime.UtcNow.AddDays(5);
+        }
+        public int Id { get; set; }
+        public string Token { get; set; } = null!;
+        public DateTime Expires { get; set; }
+
+        public int UserId { get; set; }
+        public bool IsActive => DateTime.UtcNow <= Expires;
+    }
+}
---
+++ backend/Fung.DAL/EtitiesConfiguration/RefreshTokenConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Fung.COMMON.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fung.DAL.EtitiesConfiguration
+{
+    internal class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Ignore(t => t.IsActive);
+        }
+    }
+}
