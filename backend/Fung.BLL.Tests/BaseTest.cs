2b85780c107837b38741439f15289e1dd8ba25ba
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-01 21:10:33 +0300

    Created test structure

---
+++ backend/Fung.BLL.Tests/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Fung.BLL.MappingProfiles;
+using Fung.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fung.BLL.Tests
+{
+    public class BaseTest
+    {
+        protected IMapper mapper = new Mapper(mapperConfig);
+
+        private static IConfigurationProvider mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<FuelTankMappingProfile>();
+            cfg.AddProfile<LevelIndicatorMappingProfile>();
+            cfg.AddProfile<UserMappingProfile>();
+            cfg.AddProfile<RemainingTransactionsMappingProfile>();
+            cfg.AddProfile<StationMappingProfile>();
+        });
+
+        protected DbContextOptions<DataContext> dbContextOptions = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase("TestingDB")
+            .Options;
+    }
+}
---
+++ backend/Fung.BLL.Tests/UserServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Fung.BLL.Services;
+using Fung.DAL;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Fung.BLL.Tests
+{
+    public class UserServiceTests : BaseTest
+    {
+        private readonly UserService userService;
+        private readonly DataContext context;
+        public UserServiceTests()
+        {
+            context = new DataContext(dbContextOptions);
+            userService = new UserService(context, mapper);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            
+        }
+    }
+}
