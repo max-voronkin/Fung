851fc22f9bb6ee53269a2e6bea5eac0c35a9584c
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-04 14:55:47 +0300

    Tank info page template

--- frontend/src/app/components/main-page/tank-info-page/tank-info-page.component.ts
+++ frontend/src/app/components/main-page/tank-info-page/tank-info-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { TankService } from 'src/app/services/tank.service';
+import { FuelTankInfoDTO } from 'src/models/DTO/FuelTank/fuel-tank-infoDTO';
+import { FuelTank } from 'src/models/Entities/fuel-tank';
 
 @Component({
   selector: 'app-tank-info-page',
@@ -9,12 +13,24 @@ import { ActivatedRoute } from '@angular/router';
 export class TankInfoPageComponent implements OnInit {
 
   tankId!: number;
-  constructor(private route: ActivatedRoute) { }
+  spinner = false;
+  private unsubscribe$ = new Subject<void>();
+  public tank: FuelTankInfoDTO = {} as FuelTankInfoDTO;
+  
+  constructor(private route: ActivatedRoute, private tankService: TankService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.tankId = params.get('id') as unknown as number;
     });
+
+    this.spinner = !this.spinner;
+    this.tankService.GetTank(this.tankId)
+      .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((resp) => {
+          this.tank = resp.body!
+          this.spinner = !this.spinner;
+        });
   }
 
 }
