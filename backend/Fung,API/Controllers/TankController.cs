0f88d091587f0ebc0518e262822e8b6d2a3e473d
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-04 14:56:22 +0300

    Created tank controller

---
+++ backend/Fung,API/Controllers/TankController.cs
@@ -0,0 +1,23 @@
+﻿using Fung.BLL.Services;
+using Fung.COMMON.DTO.FuelTank;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fung_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TankController : ControllerBase
+    {
+        private readonly TankService tankService;
+        public TankController(TankService tankService)
+        {
+            this.tankService = tankService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<FuelTankInfoDTO> GetTank(int id)
+        {
+            return await tankService.GetTank(id);
+        }
+    }
+}
