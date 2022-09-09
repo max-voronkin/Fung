42b744168c21016dd073ca10639c1e5c3bb4a7c6
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-09 20:37:47 +0300

    Displayed tank picture with dynamic filling

--- frontend/src/app/components/main-page/tank-info-page/tank-info-page.component.ts
+++ frontend/src/app/components/main-page/tank-info-page/tank-info-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { faBook, faChartLine, faL, faRulerVertical } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faChartLine, faFlask, faL, faRulerVertical, faTemperatureFull } from '@fortawesome/free-solid-svg-icons';
 import { Subject, takeUntil } from 'rxjs';
 import { TankService } from 'src/app/services/tank.service';
 import { FuelTankInfoDTO } from 'src/models/DTO/FuelTank/fuel-tank-infoDTO';
@@ -15,15 +16,29 @@ export class TankInfoPageComponent implements OnInit {
   tankId!: number;
   spinner = false;
   private unsubscribe$ = new Subject<void>();
+
   public tank: FuelTankInfoDTO = {} as FuelTankInfoDTO;
+
   levelTransactions : boolean = false;
   bookTransactions : boolean = false;
   graphRepresentation : boolean = false;
+  displayValueInPercentage = true;
+
   bookIcon = faBook;
   rulerIcon = faRulerVertical;
   graphIcon = faChartLine;
-  
-  constructor(private route: ActivatedRoute, private tankService: TankService) { }
+  flaskIcon = faFlask;
+  temperatureIcon = faTemperatureFull;
+
+  public get volumeInPercentage(): number {
+    return Math.round(this.tank.currentAmount / (this.tank.capacity / 100));
+  }
+
+  public get gradientStyle() {
+    return {'background-image': `linear-gradient(to top, #8AAAE5 0% ${this.volumeInPercentage}%, #ebebeb 0% ${100 - this.volumeInPercentage}%)`}
+  }
+
+  constructor(private route: ActivatedRoute, private tankService: TankService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -56,6 +71,11 @@ export class TankInfoPageComponent implements OnInit {
     this.levelTransactions = false;
     this.bookTransactions = false;
     this.graphRepresentation = true;
+  }
+
+  toggleUnits()
+  {
+    this.displayValueInPercentage = !this.displayValueInPercentage;
   }
 
 }
