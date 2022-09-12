5e49aa3036174e194a78eb003fe747f8546b8ffa
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-12 14:06:04 +0300

    Base structure for charts component

--- frontend/src/app/components/main-page/components/level-charts/level-charts.component.ts
+++ frontend/src/app/components/main-page/components/level-charts/level-charts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DropdownOption } from 'src/app/shared/fung-dropdown/fung-dropdown.component';
+import { LevelTransaction } from 'src/models/Entities/level-transaction';
 
 @Component({
   selector: 'level-charts',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LevelChartsComponent implements OnInit {
 
+  @Input() public transactions: Array<LevelTransaction> = [];
+  @Input() public tankCapacity!: number;
+  
+  public typeOptions: Array<DropdownOption> = [
+    {option: 'Volume', value: 0},
+    {option: 'Level', value: 1},
+    {option: 'Density', value: 2}
+  ];
+
+  public periodOptions: Array<DropdownOption> = [
+    {option: 'Hour', value: 0},
+    {option: '12 Hours', value: 1},
+    {option: '24 Hours', value: 2}
+  ];
+  
+  public selectedType: DropdownOption | undefined;
+  public selectedTime: DropdownOption | undefined;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeType(option: DropdownOption): void {
+    this.selectedType = option;
+  }
+
+  changeTime(option: DropdownOption): void {
+    this.selectedTime = option;
   }
 
 }
