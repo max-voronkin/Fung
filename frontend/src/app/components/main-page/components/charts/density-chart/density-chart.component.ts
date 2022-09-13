fa10fe9a884d5722e6cc9a1799cd8df40aa19460
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-13 18:12:59 +0300

    Created components for all charts

---
+++ frontend/src/app/components/main-page/components/charts/density-chart/density-chart.component.ts
@@ -0,0 +1,74 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { Subject, takeUntil } from 'rxjs';
+import { DensityPipe } from 'src/app/pipes/density.pipe';
+import { LevelTransactionService } from 'src/app/services/level-transaction.service';
+import { LevelTransaction } from 'src/models/Entities/level-transaction';
+
+@Component({
+  selector: 'density-chart',
+  templateUrl: './density-chart.component.html',
+  styleUrls: ['./density-chart.component.sass']
+})
+export class DensityChartComponent implements OnInit {
+
+  public transactions: Array<LevelTransaction> = [];
+  public tankId!: number;
+  public hours!: number;
+  public spinner: boolean = false;
+  private unsubscribe$ = new Subject<void>();
+
+  chart!: Chart;
+  private labels: (string | null)[]= [];
+  private densityData: number[] = [];
+  
+  constructor(private datePipe: DatePipe, private densityPipe: DensityPipe, private levelTransactionService: LevelTransactionService) { }
+
+  ngOnDestroy(): void {
+    this.chart.destroy();
+  }
+
+  ngOnInit(): void {
+    this.chart = new Chart('density-canvas', {
+      type: 'line',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: 'Density',
+            data: this.densityData,
+            backgroundColor: 'red',
+            borderColor: 'red',
+            fill: false
+          }
+        ]        
+      },
+      options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+    })
+  }
+
+  updateChart(tankId: number, hours: number): void {
+    this.spinner = !this.spinner;
+
+    this.levelTransactionService.GetTransactionsByPeriod(tankId, hours)
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe((resp) => {
+      this.transactions = resp.body!
+      this.spinner = !this.spinner;
+
+      this.chart.data.labels = [];
+      this.chart.data.labels?.push(...this.transactions.map(t => this.datePipe.transform(t.transactionTime, 'd/M/yy, h:mm')));
+      this.chart.data.datasets[0].data = [];
+      this.chart.data.datasets[0].data.push(...this.transactions.map(t => this.densityPipe.transform(t.density)));
+      this.chart.update();
+    })
+  }
+
+}
--- frontend/src/app/components/main-page/components/level-charts/level-charts.component.ts
+++ frontend/src/app/components/main-page/components/level-charts/level-charts.component.ts
@@ -1,42 +1,86 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { DropdownOption } from 'src/app/shared/fung-dropdown/fung-dropdown.component';
-import { LevelTransaction } from 'src/models/Entities/level-transaction';
+import { DensityChartComponent } from '../charts/density-chart/density-chart.component';
+import { HeightChartComponent } from '../charts/height-chart/height-chart.component';
+import { VolumeChartComponent } from '../charts/volume-chart/volume-chart.component';
 
 @Component({
   selector: 'level-charts',
   templateUrl: './level-charts.component.html',
   styleUrls: ['./level-charts.component.sass']
 })
-export class LevelChartsComponent implements OnInit {
+export class LevelChartsComponent implements OnInit, AfterViewInit {
 
-  @Input() public transactions: Array<LevelTransaction> = [];
   @Input() public tankCapacity!: number;
+  @Input() public tankId!: number;
+
+  @ViewChild(VolumeChartComponent) volumeChart?: VolumeChartComponent;
+  @ViewChild(HeightChartComponent) heightChart?: HeightChartComponent;
+  @ViewChild(DensityChartComponent) densityChart?: DensityChartComponent;
   
   public typeOptions: Array<DropdownOption> = [
     {option: 'Volume', value: 0},
-    {option: 'Level', value: 1},
+    {option: 'Height', value: 1},
     {option: 'Density', value: 2}
   ];
 
   public periodOptions: Array<DropdownOption> = [
-    {option: 'Hour', value: 0},
-    {option: '12 Hours', value: 1},
-    {option: '24 Hours', value: 2}
+    {option: '1 Hour', value: 1},
+    {option: '12 Hours', value: 12},
+    {option: '24 Hours', value: 24}
   ];
   
   public selectedType: DropdownOption | undefined;
   public selectedTime: DropdownOption | undefined;
   constructor() { }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.volumeChart?.updateChart(this.tankId, this.selectedTime!.value);
+    }, 0)
+  }
+
   ngOnInit(): void {
   }
 
   changeType(option: DropdownOption): void {
     this.selectedType = option;
+    if (option.value === 0)
+    {
+      setTimeout(() => {
+        this.volumeChart?.updateChart(this.tankId, this.selectedTime!.value);
+      }, 0)     
+    }
+    else if (option.value === 1)
+    {
+      setTimeout(() => {
+        this.heightChart?.updateChart(this.tankId, this.selectedTime!.value);
+      }, 0)   
+    }
+    else 
+    {
+      setTimeout(() => {
+        this.densityChart?.updateChart(this.tankId, this.selectedTime!.value);
+      }, 0)   
+    }
+    
   }
 
   changeTime(option: DropdownOption): void {
     this.selectedTime = option;
+    //this.volumeChart?.updateChart(this.tankId, this.selectedTime.value);
+    if (this.selectedType!.value === 0)
+    {
+      this.volumeChart?.updateChart(this.tankId, this.selectedTime!.value);
+    }
+    else if (this.selectedType!.value === 1)
+    {
+      this.heightChart?.updateChart(this.tankId, this.selectedTime!.value);
+    }
+    else 
+    {
+      this.densityChart?.updateChart(this.tankId, this.selectedTime!.value);
+    }
   }
 
 }
