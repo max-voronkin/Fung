4fdeb8302e4d6392c7b71966f4113c93d0828b7b
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-16 12:44:48 +0300

    Created settings module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MainPageModule } from './pages/main-page/main-page.module';
 import { JwtInterceptor } from './services/helpers/jwt.interceptor';
 import { ErrorInterceptor } from './services/helpers/error.interceptor';
 import { NgChartsModule } from 'ng2-charts';
+import { SettingsPageModule } from './pages/settings-page/settings-page.module';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { NgChartsModule } from 'ng2-charts';
     AuthModule,
     HttpClientModule,
     MainPageModule,
-    NgChartsModule
+    NgChartsModule,
+    SettingsPageModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
