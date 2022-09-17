69937a82749ceb1d0dfd015a987d39bbccb84b00
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-17 16:24:48 +0300

    On settings init getting current user, and pass next

--- frontend/src/app/pages/settings-page/settings-page/settings-page.component.ts
+++ frontend/src/app/pages/settings-page/settings-page/settings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/models/Entities/user';
 
 @Component({
   selector: 'app-settings-page',
@@ -9,9 +11,12 @@ export class SettingsPageComponent implements OnInit {
 
   public spinner = false;
 
-  constructor() { }
+  public currentUser!: User | null;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.currentUser = this.userService.getUser();
   }
 
 }
