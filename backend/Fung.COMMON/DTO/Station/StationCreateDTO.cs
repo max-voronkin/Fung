aaddabe60ec47016d1cbc1ea080deef1699d46a9
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-17 17:28:07 +0300

    Fixed typo

--- backend/Fung.COMMON/DTO/Station/StationCreateDTO.cs
+++ backend/Fung.COMMON/DTO/Station/StationCreateDTO.cs
@@ -2,7 +2,7 @@
 {
     public sealed class StationCreateDTO
     {
-        public int userId { get; set; }
+        public int UserId { get; set; }
         public string Name { get; set; } = null!;
     }
 }
