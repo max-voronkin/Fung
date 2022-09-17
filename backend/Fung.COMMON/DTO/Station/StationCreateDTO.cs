0f12b6e71f1ebf4b164608b9218eb031444f1406
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-17 17:20:19 +0300

    Created  new station dto

---
+++ backend/Fung.COMMON/DTO/Station/StationCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Fung.COMMON.DTO.Station
+{
+    public sealed class StationCreateDTO
+    {
+        public int userId { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
