8093f6c52aaba5f1c33999eaaed5e769895c52a7
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-18 18:59:09 +0300

    Created input controller to accept values

---
+++ backend/Fung,API/Controllers/InputController.cs
@@ -0,0 +1,31 @@
+﻿using Fung.COMMON.DTO.FuelTank;
+using Fung.COMMON.DTO.LevelIndicator;
+using Microsoft.AspNetCore.Mvc;
+using Fung.BLL.Services;
+
+namespace Fung_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InputController : ControllerBase
+    {
+        private readonly InputService inputService;
+        public InputController(InputService inputService)
+        {
+            this.inputService = inputService;
+        }
+
+        [HttpPost("level/{token}")]
+        public async Task<IActionResult> PostLevelTransaction(string token, [FromBody] LevelIndicatorTransactionCreateDTO newTransactionDTO)
+        {
+            await inputService.CreateLevelTransaction(token, newTransactionDTO);
+            return Ok();
+        }
+
+        [HttpPost("tank/{token}")]
+        public async Task<FuelTankDTO> CreateTank(string token, [FromBody] FuelTankCreateDTO newTankDTO)
+        {
+            return await inputService.CreateTank(token, newTankDTO);
+        }
+    }
+}
