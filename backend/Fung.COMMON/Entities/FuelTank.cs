622664f580cd086c9a21f760ab117b43721e8245
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-18 18:59:26 +0300

    Default value of UpdatedAt

--- backend/Fung.COMMON/Entities/FuelTank.cs
+++ backend/Fung.COMMON/Entities/FuelTank.cs
@@ -7,7 +7,7 @@
         public int Number { get; set; }
         public double Capacity { get; set; }
         public double CurrentAmount { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
         public int StationId { get; set; }
 
         public ICollection<LevelIndicatorTransactions>? LevelTransactions{ get; set; }
