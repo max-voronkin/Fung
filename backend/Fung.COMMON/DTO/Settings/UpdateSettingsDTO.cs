62e7be3ea4a32a0cb6864d79328da49c5ab89725
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-29 14:56:43 +0300

    Dto for settings update

---
+++ backend/Fung.COMMON/DTO/Settings/UpdateSettingsDTO.cs
@@ -0,0 +1,12 @@
+﻿using Fung.COMMON.Enums;
+
+namespace Fung.COMMON.DTO.Settings
+{
+    public sealed class UpdateSettingsDTO
+    {
+        public int Id { get; set; }
+        public HeightUnit HeightUnit { get; set; } = HeightUnit.Meter;
+        public VolumeUnit VolumeUnit { get; set; } = VolumeUnit.Liter;
+        public int UserId { get; set; }
+    }
+}
