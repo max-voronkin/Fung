b7220c6af4b49dce147f1993214b2261ce3d236a
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-29 15:00:45 +0300

    Updated to have settings

--- frontend/src/models/DTO/User/user-authDTO.ts
+++ frontend/src/models/DTO/User/user-authDTO.ts
@@ -1,5 +1,10 @@
+import { Settings } from "src/models/Entities/settings"
+import { User } from "src/models/Entities/user"
+
 export interface AuthUserDTO 
 {
     accessToken: string
     refreshToken: string
+    user: User
+    settings: Settings
 }
