7a092f10ab1f120b964f9bab3638b429f2f08eec
Max Voronkin <voronkin.max.wm@gmail.com>
2022-09-29 14:59:25 +0300

    Updated to use units from settings

--- frontend/src/app/pipes/volume.pipe.ts
+++ frontend/src/app/pipes/volume.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { VolumeUnit } from 'src/models/enums/volume-unit';
+import { DataService } from '../services/data.service';
 
 @Pipe({
   name: 'volume'
 })
 export class VolumePipe implements PipeTransform {
+  constructor (private dataService: DataService) {}
 
   transform(value: number): number {
-    return Math.round((value * 1000) * 10) / 10;
+    switch (this.dataService.volumeUnits!) {
+      case VolumeUnit.CubicMetre: {
+        return Math.round(value * 100) / 100;
+      }
+      case VolumeUnit.Liter: {
+        return Math.round((value * 1000) * 10) / 10;
+      }
+    }  
   }
 
 }
