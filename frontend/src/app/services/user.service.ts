e533066eebb4516c29efd9e531f83bd90b79053f
Max Voronkin <voronkin.max.wm@gmail.com>
2022-10-26 13:38:45 +0300

    Created method to update password

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,31 +1,20 @@
 import { Injectable } from '@angular/core';
+import { UserChangePasswordDTO } from 'src/models/DTO/User/user-changePasswordDTO';
 import { User } from 'src/models/Entities/user';
 import { HttpService } from './http.service';
-import { JwtService } from './jwt.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private jwtService: JwtService, private httpService: HttpService) { }
+  constructor(private httpService: HttpService) { }
 
   public routePrefix = '/api/user';
 
-  // public getUser() {
-  //   const accessToken = JSON.parse(localStorage.getItem('accessToken')!);
-
-  //   if (accessToken)
-  //   {
-      
-  //     let info : any = this.jwtService.DecodeToken(accessToken);
-  //     let user = {} as User;
-  //     user.email = info.email;
-  //     user.id = info.userId;
-  //     return user;
-  //   }
-  //   return null;
-  // }
+  public updatePassword(payload: UserChangePasswordDTO) {
+    return this.httpService.putFullRequest<void>(`${this.routePrefix}/updatePassword`, payload);
+  }
 
   public getUserFromToken() {
     return this.httpService.getFullRequest<User>(`${this.routePrefix}/fromToken`);
