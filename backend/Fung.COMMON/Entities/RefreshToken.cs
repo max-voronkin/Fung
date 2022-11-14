7616ef945e4df1ac5b2755ea82a142e8ef9c9fbb
Max Voronkin <voronkin.max.wm@gmail.com>
2022-11-14 16:36:27 +0200

    Changed RefreshToken to Token entity to store different tokens

--- backend/Fung.COMMON/Entities/RefreshToken.cs
+++ backend/Fung.COMMON/Entities/RefreshToken.cs
@@ -1,16 +1,19 @@
-﻿namespace Fung.COMMON.Entities
+﻿using Fung.COMMON.Enums;
+
+namespace Fung.COMMON.Entities
 {
-    public sealed class RefreshToken
+    public sealed class Token
     {
-        public RefreshToken()
+        public Token()
         {
             Expires = DateTime.UtcNow.AddDays(5);
         }
         public int Id { get; set; }
-        public string Token { get; set; } = null!;
+        public string Value { get; set; } = null!;
         public DateTime Expires { get; set; }
 
         public int UserId { get; set; }
         public bool IsActive => DateTime.UtcNow <= Expires;
+        public TokenType Type { get; set; }
     }
 }
