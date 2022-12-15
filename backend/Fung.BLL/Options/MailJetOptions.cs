10a72f2c6dca0e76cf0d730fb37d17fcf7f7d1a3
Max Voronkin <voronkin.max.wm@gmail.com>
2022-12-15 15:51:52 +0200

    Created Mailjet options model

---
+++ backend/Fung.BLL/Options/MailJetOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Fung.BLL.Options
+{
+    public sealed class MailJetOptions
+    {
+        public string ApiKey { get; set; } = null!;
+        public string ApiSecret { get; set; } = null!;
+        public bool IsSandboxMode { get; set; }
+    }
+}
