eb124acd0011fd7c5c67e1036849fbbdeb8dd6b3
Max Voronkin <voronkin.max.wm@gmail.com>
2022-12-15 15:52:20 +0200

    Created MailJet service

---
+++ backend/Fung.BLL/Services/Email/MailJetEmailService.cs
@@ -0,0 +1,36 @@
+﻿using Fung.BLL.Email;
+using Fung.BLL.Options;
+using Fung.COMMON.Entities;
+using Mailjet.Client;
+using Mailjet.Client.TransactionalEmails;
+
+namespace Fung.BLL.Services.Email
+{
+    public class MailJetEmailService : BaseEmailService, IEmailService
+    {
+        private readonly IMailjetClient client;
+        private readonly MailJetOptions mailJetOptions;
+        public MailJetEmailService(IMailjetClient client, EmailOptions options, MailJetOptions mailJetOptions) : base(options)
+        {
+            this.client = client;
+            this.mailJetOptions = mailJetOptions;
+        }
+
+        public async Task SendEmailAsync(string recipient, Token token)
+        {
+            var email = await BuildEmailAsync(recipient, token);
+
+            var response = await client.SendTransactionalEmailAsync(email, isSandboxMode: mailJetOptions.IsSandboxMode);
+        }
+
+        private async Task<TransactionalEmail> BuildEmailAsync(string recipient, Token token)
+        {
+            return new TransactionalEmailBuilder()
+                .WithFrom(new SendContact(emailOptions.SenderEmail))
+                .WithTo(new SendContact(recipient))
+                .WithSubject(GetSubject(token))
+                .WithHtmlPart(await GetContent(token))
+                .Build();
+        }
+    }
+}
